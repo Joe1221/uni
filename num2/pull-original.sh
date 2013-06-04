8aca1debae21b0564da2d6119890d69f9ac3f8db
Stephan Hilb <stephan@ecshi.net>
2013-06-04 21:30:21 +0200

    num2: bash skript zum aktualisieren des num2-original branches

---
+++ num2/pull-original.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+URL="http://www.mathematik.uni-stuttgart.de/~harrach/lehre/Numerik2.tex";
+target="Numerik2.tex";
+
+git checkout num2-original
+if [[ $? != 0 ]]; then
+	exit $?;
+fi
+
+
+# Getting tex file
+echo "Fetching original tex from $URL to $target … ";
+wget -qO "$target" "$URL";
+
+# charset conversion
+CHARSET="$( file -bi "$target"|awk -F "=" '{print $2}')"
+if [ "$CHARSET" != "utf-8" ]; then
+	echo "Converting from $CHARSET to utf8 …";
+
+	iconv -f "$CHARSET" -t utf8 "$target" -o "${target}.tmp";
+	mv "${target}.tmp" "${target}";
+fi
+
+# line-endings conversion
+echo "Stripping carriage-returns …";
+tr -d '\r' < "${target}" > "${target}.tmp";
+mv "${target}.tmp" "${target}";
+
+
+# Änderungen commiten
+git add "${target}" &&
+git commit -m "num2: Änderungen des Orginalskripts" &&
+git push origin num2-original:num2-original;
+git checkout master;
