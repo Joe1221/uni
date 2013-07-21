8688a43dcd8e4e8a57562b06c8298851cc8a6074
Stephan Hilb <stephan@ecshi.net>
2013-07-21 17:44:43 +0200

    num2: pull-original skript etwas verbessert

--- num2/pull-original.sh
+++ num2/pull-original.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 
 URL="http://www.mathematik.uni-stuttgart.de/~harrach/lehre/Numerik2.tex";
-target="Numerik2.tex";
+TARGET="Numerik2.tex";
 
-git checkout num2-original
+git stash -q && \
+git checkout -q num2-original && \
+git pull --ff-only;
 if [[ $? != 0 ]]; then
 	exit $?;
 fi
-git pull --ff-only;
 
 
 # Getting tex file
-echo "Fetching original tex from $URL to $target … ";
-wget -qO "$target" "$URL";
+echo "Fetching original tex from $URL to $TARGET … ";
+wget -qO "$TARGET" "$URL";
+if [ $? -eq 0 ]; then
 
-# charset conversion
-CHARSET="$( file -bi "$target"|awk -F "=" '{print $2}')"
-if [ "$CHARSET" != "utf-8" ]; then
-	echo "Converting from $CHARSET to utf8 …";
+	# charset conversion
+	CHARSET="$( file -bi "$TARGET"|awk -F "=" '{print $2}')"
+	if [ "$CHARSET" != "utf-8" ]; then
+		echo "Converting from $CHARSET to utf8 …";
 
-	iconv -f "$CHARSET" -t utf8 "$target" -o "${target}.tmp";
-	mv "${target}.tmp" "${target}";
+		iconv -f "$CHARSET" -t utf8 "$TARGET" -o "${TARGET}.tmp";
+		mv "${TARGET}.tmp" "${TARGET}";
+	fi
+
+	# line-endings conversion
+	echo "Stripping carriage-returns …";
+	tr -d '\r' < "${TARGET}" > "${TARGET}.tmp";
+	mv "${TARGET}.tmp" "${TARGET}";
+
+	git diff --quiet;
+	if [ $? -ne 0 ]; then
+		echo "There were changes, committing …"
+		# Änderungen commiten
+		git add "${TARGET}" && \
+		git commit -m "num2: Änderungen des Orginalskripts" && \
+		git push origin num2-original:num2-original;
+	else
+		echo "There were no changes, finishing …"
+	fi
+
+else
+	echo "ERROR: Couldn't fetch $URL.";
 fi
-
-# line-endings conversion
-echo "Stripping carriage-returns …";
-tr -d '\r' < "${target}" > "${target}.tmp";
-mv "${target}.tmp" "${target}";
-
-
-# Änderungen commiten
-git add "${target}" &&
-git commit -m "num2: Änderungen des Orginalskripts" &&
-git push origin num2-original:num2-original;
-git checkout master;
+git checkout -q master && \
+git stash pop -q;
