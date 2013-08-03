d69281ed13a0cbe66d0f6e4e1464ff1873a5bbfd
Stephan Hilb <stephan@ecshi.net>
2013-08-03 15:27:19 +0200

    make: use latexmk if available

--- common/pdflatex.mk
+++ common/pdflatex.mk
@@ -1,18 +1,55 @@
 # TARGET has to be specified
 
+PDFLATEX_OPTIONS += -synctex=1
+LATEXMK     ?= latexmk -recorder -pdf -pdflatex="pdflatex $(PDFLATEX_OPTIONS) %O %S"
+PDFVIEWER   ?= xdg-open
 PDFLATEX    ?= pdflatex -synctex=1
 BIBTEX      ?= bibtex
-PDFVIEWER   ?= xdg-open
 
 ifneq ($(strip $(TARGET)),)
 PDFTARGETS  += $(TARGET).pdf
 endif
 
-INCLUDEDIR   := $(shell git rev-parse --show-toplevel)/common
+export TEXINPUTS = $(shell git rev-parse --show-toplevel)/common:
 
-#=================================
+LATEXMK_VERSION := $(shell latexmk --version 2> /dev/null)
+
+
+all: $(PDFTARGETS)
+
+view: $(PDFTARGETS)
+	$(PDFVIEWER) $(PDFTARGETS)
+
+
+ifdef LATEXMK_VERSION
+#================================
+# Using latexmk
+#--------------------------------
+$(info latexmk installed.)
+
+
+.PHONY: FORCE_MAKE
+
+%.pdf: %.tex FORCE_MAKE
+	$(LATEXMK) $<
+
+#clean:
+#	$(LATEXMK) -c $<
+#
+#distclean: clean
+#	$(LATEXMK) -C
+
+
+
+else
+#================================
+# Using pdflatex with make rules
+#--------------------------------
+$(info latexmk not installed, falling back to pdflatex make rules.)
+
+
+
 # grep bibtex dependencies
-#---------------------------------
 ifneq ($(strip $(TARGET)),)
 BIBFILES = $(patsubst %,%.bib,\
 		$(shell grep '^[^%]*\\bibliography{' $(TARGET).tex | \
@@ -21,9 +58,8 @@ BIBFILES = $(patsubst %,%.bib,\
 endif
 
 
-#=================================
-# \input and \include dependencies
 #---------------------------------
+# \input and \include dependencies
 ifneq ($(strip $(TARGET)),)
 INCLUDEDTEX := $(patsubst %,%.tex,\
 		$(shell sed -rn 's/^[^%]*\\(input|include)\{([^\.\}]*)(\.tex)?\}/\2/p' $(TARGET).tex))
@@ -35,7 +71,7 @@ endif
 endif
 # quick-hack to get sty dependency (TODO: a clean solution)
 INCLUDEDPKG = $(wildcard $(INCLUDEDIR)/*.sty) $(wildcard $(INCLUDEDIR)/*.cls)
-#=================================
+#---------------------------------
 
 AUXFILES = $(PDFTARGETS:.pdf=.aux)
 AUXFILES += $(INCLUDEDTEX:.tex=.aux)
@@ -46,40 +82,42 @@ REVISION := $(shell git rev-parse --short HEAD)
 
 .PHONY: all clean distclean pdf view
 
-all: pdf $(AFTERALL)
-
-pdf: $(PDFTARGETS)
-
-view: $(PDFTARGETS)
-	$(PDFVIEWER) $(PDFTARGETS)
-
 # to generate aux but not pdf from pdflatex, use -draftmode
 .SECONDARY: $(AUXFILES)
-%.aux: | %.tex
-	(TEXINPUTS=.:$(INCLUDEDIR):$(TEXINPUTS) $(PDFLATEX) -draftmode $*);
+
+$(AUXFILES): %.aux: | %.tex
+	$(PDFLATEX) -draftmode $*;
 
 # introduce BibTeX dependency if we found a \bibliography
 ifneq ($(strip $(BIBFILES)),)
 BIBDEPS = %.bbl
-%.bbl: %.aux $(BIBFILES)
+%.bbl: $(BIBFILES) %.aux
 	$(BIBTEX) $*
 endif
 
 #$(PDFTARGETS): %.pdf: %.tex %.aux $(BIBDEPS) $(INCLUDEDTEX)
-$(PDFTARGETS): %.pdf: %.tex $(BIBDEPS) $(INCLUDEDTEX) $(INCLUDEDPKG)
-	(TEXINPUTS=.:$(INCLUDEDIR):$(TEXINPUTS) $(PDFLATEX) $*)
+$(PDFTARGETS): %.pdf: %.tex $(INCLUDEDTEX) $(INCLUDEDPKG) | $(BIBDEPS)
+	$(PDFLATEX) $*
 ifneq ($(strip $(BIBFILES)),)
 	@if grep -q "undefined references" $*.log; then \
-		(TEXINPUTS=.:$(INCLUDEDIR):$(TEXINPUTS) $(BIBTEX) $*) && (TEXINPUTS=.:$(INCLUDEDIR):$(TEXINPUTS) $(PDFLATEX) $*); fi
+		$(BIBTEX) $* && $(PDFLATEX) $*; fi
 endif
 	@while grep -q "Rerun to" $*.log; do \
-		(TEXINPUTS=.:$(INCLUDEDIR):$(TEXINPUTS) $(PDFLATEX) $*); done
+		$(PDFLATEX) $*; done
+
+
+
+
+endif
+
 
 clean:
 	rm -f $(foreach T,$(PDFTARGETS:.pdf=), \
 		$(T).out $(T).thm $(T).blg $(T).bbl \
 		$(T).lof $(T).lot $(T).toc $(T).idx \
-		$(T).nav $(T).snm $(T)-pics.pdf) \
+		$(T).nav $(T).snm $(T)-pics.pdf \
+		$(T).fdb_latexmk $(T).synctex.gz \
+		$(T).log $(T).fls $(T).aux) \
 		$(AUXFILES) $(LOGFILES)
 
 distclean: clean
