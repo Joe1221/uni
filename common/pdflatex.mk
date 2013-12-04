c54912dc92277b89eec360de0deeb6a6fc202430
Stephan Hilb <stephan@ecshi.net>
2013-12-04 11:20:37 +0100

    make,ana3: compile fix

--- common/pdflatex.mk
+++ common/pdflatex.mk
@@ -8,7 +8,7 @@ LATEXMK     ?= latexmk -pdf -e '$$pdflatex=q/pdflatex $(PDFLATEX_OPTIONS) %O %S/
 LATEXMK   ?= latexmk -pdf
 
 PDFVIEWER ?= xdg-open
-PDFLATEX  ?= pdflatex -synctex=1               # Deprecated
+PDFLATEX  ?= pdflatex $(PDFLATEX_OPTIONS)               # Deprecated
 BIBTEX    ?= bibtex                            # Deprecated
 
 TEXCONFIG ?= config.tex
