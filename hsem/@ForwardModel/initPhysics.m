c98de0e173a335c0008d7cf46a9b42c785cb0d13
Stephan Hilb <stephan@ecshi.net>
2014-01-05 19:02:00 +0100

    hsem: remove comments

--- hsem/@ForwardModel/initPhysics.m
+++ hsem/@ForwardModel/initPhysics.m
@@ -11,13 +11,6 @@ function initPhysics (obj)
 	% Randbedingung für C (u = f)
 	ph_bnd_C = obj.phys.feature.create(strcat(obj.C.name, '_dirichlet'), 'DirichletBoundary', 1);
 	ph_bnd_C.selection.named(strcat(obj.geom_name, '_', obj.Omega.name, '_bnd'));
-	%ph_bnd_C.set('r', 'x^2-2*y+2');
-	%ph_bnd_C.set('r', 'cos(3*polar.phi) + 2*sin(polar.phi)');
-	%ph_bnd_C.set('r', 'f(polar.r, polar.phi)');
-	%obj.setFValue('cos(3*polar.phi) + 2*sin(polar.phi)');
-	%ph_neu_C = obj.phys.feature.create(strcat(obj.C.name, '_neumann'), 'NeumannBoundary', 1);
-	%ph_neu_C.selection.named(strcat(obj.geom_name, '_', obj.Omega.name, '_bnd'));
-	%ph_bnd_C.set('r', 'f(polar.r, polar.phi)');
 
 	% Randbedingung für Gamma (u = 0)
 	ph_bnd_Gamma = obj.phys.feature.create(obj.Gamma.name, 'DirichletBoundary', 1);
