88f9f03d2fe692fcd9ff1dd1b16622b3ab5392dc
Stephan Hilb <stephan@ecshi.net>
2015-01-09 13:22:59 +0100

    bth: fixes

--- bth/code/ramd.cpp
+++ bth/code/ramd.cpp
@@ -126,15 +126,11 @@ Rational calc_idx (const Index<T, R>& index) {
 
 template<class R>
 std::vector<Polynomial<R>> prem_seq (Polynomial<R> p, Polynomial<R> q) {
-    /*if (p.degree() < q.degree())
-        std::swap(p, q);*/
 
     std::vector<Polynomial<R>> prem_seq = {p, q};
     int d = p.degree() - q.degree() + 1;
 
     while (q.degree() > 0) {
-        //std::cout << "start it" << std::endl;
-        //std::cout << p << std::endl;
         int d = p.degree() - q.degree();
 
         int k;
@@ -144,15 +140,13 @@ std::vector<Polynomial<R>> prem_seq (Polynomial<R> p, Polynomial<R> q) {
             ++k, d = p.degree() - q.degree()
         ) {
             R plead = p.lead();
-            //std::cout << p << std::endl;
+
             p *= q.lead();
-            //std::cout << p << std::endl;
             p -= plead * q * Polynomial<R>::Monomial(d);
-            //std::cout << p << std::endl;
         }
         if (k++ & 1)
             p *= q.lead();
-        //std::cout << "end it" << std::endl;
+
         prem_seq.push_back(p);
         std::swap(p, q);
     }
