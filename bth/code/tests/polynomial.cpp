b35e91db39f5bca307ed6d35456d39348a864fd4
Stephan Hilb <stephan@ecshi.net>
2015-01-15 14:14:55 +0100

    bth: test: polynomial multiplication

--- bth/code/tests/polynomial.cpp
+++ bth/code/tests/polynomial.cpp
@@ -4,8 +4,18 @@
 #include "../Polynomial.h"
 
 TEST_CASE( "Polynomial Initialization", "[polynomial]" ) {
-	auto p = Polynomial<Integer> {5, 3, 1};
+    auto p = Polynomial<Integer> {5, 3, 1};
 
-	REQUIRE( p.degree() == 2 );
+    REQUIRE( p.degree() == 2 );
 }
 
+
+TEST_CASE( "Polynomial Multiplication", "[polynomial]" ) {
+    auto p = Polynomial<Integer> {5, 3, 1};
+    auto q = Polynomial<Integer> {4, 2, 7};
+    auto product = p * q;
+
+    auto r = Polynomial<Integer> {20, 22, 45, 23, 7};
+
+    REQUIRE( product == r );
+}
