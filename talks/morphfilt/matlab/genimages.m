84584b5216b411dd01116ccc07876043f43a79d2
Stephan Hilb <stephan@ecshi.net>
2015-05-12 23:38:03 +0200

    talks/morphfilt: presentation hopefully finished

--- talks/morphfilt/matlab/genimages.m
+++ talks/morphfilt/matlab/genimages.m
@@ -2,11 +2,41 @@ im = phantom(80);
 
 imwrite(im, 'images/phantom80.png');
 
-
 se = strel('rectangle', [3,3]);
 imwrite(im, 'images/phantom80.png');
 imwrite(imerode(im, se), 'images/phantom80_erode.png');
 imwrite(imdilate(im, se), 'images/phantom80_dilate.png');
 
+im = imread('images/worms80.png');
+se = strel('diamond', 1);
+imwrite(imerode(im, se), 'images/worms80_erode.png');
+imwrite(imdilate(im, se), 'images/worms80_dilate.png');
+imwrite(imopen(im, se), 'images/worms80_open.png');
+imwrite(imclose(im, se), 'images/worms80_close.png');
 
+im = imread('images/fish15.png');
+se = strel('rectangle', [2, 2]);
+imwrite(imerode(im, se), 'images/fish15_erode.png');
+imwrite(imdilate(im, se), 'images/fish15_dilate.png');
+imwrite(imopen(im, se), 'images/fish15_open.png');
+imwrite(imclose(im, se), 'images/fish15_close.png');
 
+im = imread('images/leopard250.png');
+se = strel('rectangle', [10, 10]);
+imwrite(real(im > 130), 'images/leopard250_thres.png');
+imwrite(imopen(im, se), 'images/leopard250_open.png');
+imwrite(imclose(im, se), 'images/leopard250_close.png');
+imwrite(im - imopen(im, se), 'images/leopard250_wtophat.png');
+imwrite(imclose(im, se) - im, 'images/leopard250_btophat.png');
+imwrite(real(im - imopen(im, se) > 5), 'images/leopard250_wtophat_thres.png');
+imwrite(real(imclose(im, se) - im < 60), 'images/leopard250_btophat_thres.png');
+
+im = imread('images/chair.png');
+se = ones(5,5);
+imwrite(imfilter(im,se/25), 'images/chair_linmedian.png');
+imwrite(ordfilt2(im,13,se), 'images/chair_morphmedian.png');
+
+im = imread('images/rects.png');
+s1 = strel('arbitrary', [0, 0, 0; 0, 1, 1; 0, 1, 0]);
+s2 = strel('arbitrary', [0, 1, 1; 1, 0, 0; 1, 0, 0]);
+imwrite(min(imerode(im, s1), imerode(imcomplement(im),s2)), 'images/rects_hitmiss.png');
--- talks/morphfilt/matlab/test.m
+++ talks/morphfilt/matlab/test.m
@@ -1,5 +1,5 @@
 function test (impath, s)
-    im = rgb2gray(imread(impath));
+    im = imread(impath);
 
     subplot(4,3,1), imshow(im, 'InitialMagnification', 'fit');
     subplot(4,3,4), imshow(imdilate(im, s));
@@ -8,12 +8,20 @@ function test (impath, s)
     subplot(4,3,8), imshow(imclose(im, s));
     subplot(4,3,10), imshow(im - imopen(im, s)); % white-top-hat
     subplot(4,3,11), imshow(imclose(im, s) - im); % black-top-hat
-    im2 = im - imopen(im, s);
-    im3 = imclose(im, s) - im;
-    subplot(4,3,12), imshow(real(im2 > 5)); % black-top-hat
-    subplot(4,3,6), imshow(imclose(im3, s) - im3); % black-top-hat
-    subplot(4,3,3), imshow(im2 - imopen(im2,s)); % black-top-hat
-    %subplot(4,3,3), imshow(real(im > 130)); % black-top-hat
-    im2
-    %spy(im2)
+
+
+
+    %subplot(4,3,6), imshow(imfilter(im,ones(5,5)/25));
+    %subplot(4,3,9), imshow(ordfilt2(im,13,ones(5,5)));
+    %subplot(4,3,12), imshow(ordfilt2(im,3,[0,1,0;1,1,1;0,1,0]));
+
+
+    %im2 = im - imopen(im, s);
+    %im3 = imclose(im, s) - im;
+    %%subplot(4,3,12), imshow(real(im2 > 5)); % black-top-hat
+    %subplot(4,3,6), imshow(imclose(im3, s) - im3); % black-top-hat
+    %subplot(4,3,3), imshow(im2 - imopen(im2,s)); % black-top-hat
+    %%subplot(4,3,3), imshow(real(im > 130)); % black-top-hat
+    %im2
+    %%spy(im2)
 end
