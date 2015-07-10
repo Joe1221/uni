5ee6236c572b833fe93368c30dcd5b623a6f5a48
Stephan Hilb <stephan@ecshi.net>
2015-07-10 15:52:43 +0200

    common/matlab: reindent, 4 spaces

--- common/matlab/zeroshift.m
+++ common/matlab/zeroshift.m
@@ -1,16 +1,16 @@
 function B = zeroshift (A, k)
-	B = circshift(A, k);
+    B = circshift(A, k);
 
-	for i = 1:numel(k)
-		if (abs(k(i)) >= size(A,i))
-			B = zeros(size(A));
-			break
-		end
-		if (k(i) > 0)
-			B(1:k(i),:) = 0;
-		elseif (k(i) < 0)
-			B(mod(k(i),size(A,i))+1:end,:) = 0;
-		end
-		B = shiftdim(B, 1);
-	end
+    for i = 1:numel(k)
+        if (abs(k(i)) >= size(A,i))
+            B = zeros(size(A));
+            break
+        end
+        if (k(i) > 0)
+            B(1:k(i),:) = 0;
+        elseif (k(i) < 0)
+            B(mod(k(i),size(A,i))+1:end,:) = 0;
+        end
+        B = shiftdim(B, 1);
+    end
 end
